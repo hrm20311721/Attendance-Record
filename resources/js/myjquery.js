1eb73724ed910d7148158e997458448e51281d14
hrm2031 <hrm20311721@gmail.com>
2021-12-03 14:05:30 +0900

    update: Bootstrapアップデートに伴うAttr修正

--- resources/js/myjquery.js
+++ resources/js/myjquery.js
@@ -46,7 +46,7 @@ window.onload = function () {
 
             $.ajax({
                 type: "GET",
-                url: "/grades",
+                url: "grades",
                 data: { 'grade': grade_id },
                 dataType: "json"
             }).done(function (data) {
@@ -56,7 +56,7 @@ window.onload = function () {
                 data = data.data;
                 //optionにセット
                 $.each(data, function (key, value) {
-                    $('#record_kids').append($('<a>').text(value.name).attr({ 'class': "list-group-item list-group-item-action", 'data-toggle': "modal", 'data-target': "#create-record", 'role': "tab", 'value': value.id, }));
+                    $('#record_kids').append($('<a>').text(value.name).attr({ 'class': "list-group-item list-group-item-action", 'data-toggle': "modal", 'data-bs-target': "#create-record", 'role': "tab", 'value': value.id, }));
                 });
             }).fail(function (res) {
                 var res = res.responseJSON.errors;
