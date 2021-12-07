9b967b2e5122507cdfdd3cde7f585516101abb86
hrm2031 <hrm20311721@gmail.com>
2021-12-07 16:40:23 +0900

    improve: レコード一覧のモーダルのビューを改善
    レスポンシブ対応になっていなかったため

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -4,8 +4,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <table class="table table-striped table-responsive-lg">
+        <div class="col-md-12 table-responsive">
+            <table class="table table-striped ">
                 <thead>
                     <tr>
                         <th scope="col">日付</th>
@@ -71,57 +71,51 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body" id="modal-body">
-                    <form class="mb-0">
-                        <!-- 園児名とクラスは更新できない -->
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h6 for="date" class="record-date"></h6>
-                            </div>
+                    <!-- 園児名とクラスは更新できない -->
+                    <h6 for="date" class="record-date"></h6>
+                    <div class="row px-3">
+                        <div class="col-md-6 d-flex align-items-center mb-3 p-0">
+                            <label for="grade_name" class="form-label me-2 my-0 p-0">クラス：</label>
+                            <p class="text-secondary record-grade my-0" id="grade"></p>
                         </div>
-                        <div class="form-row">
-                            <div class="form-group align-items-center col-md-6">
-                                <label for="grade_name" class="col-formLabel text-md-left">クラス：</label>
-                                <p class="text-secondary record-grade" id="grade"></p>
-                            </div>
-                            <div class="form-group align-items-center col-md-6">
-                                <label for="kid_name" class="col-formLabel text-md-left">園児名：</label>
-                                <p class="text-secondary record-kid"></p>
-                            </div>
+                        <div class="col-md-6 d-flex align-items-center mb-3 p-0">
+                            <label for="kid_name" class="form-label me-2 my-0">園児名：</label>
+                            <p class="text-secondary record-kid my-0"></p>
                         </div>
-                        <!-- 送りに来た人と時間 -->
-                        <div class="form-row">
-                            <!--　送りに来た人 -->
-                            <div class="form-group align-items-center col-md-6 do_guardian">
-                                <label for="do_guardian_id" class="col-form-label text-md-left">お送りに来た人の名前</label>
-                                <select class="form-control-md p-2" aria-label="Default select" name="do_guardian_id">
-                                </select>
-                            </div>
-                            <!-- 送りに来た時間 -->
-                            <div class="form-group align-items-center col-md-6 do_time">
-                                <label for="do_time" class="col-form-label text-md-left">お送りに来た時間</label>
-                                <input class="form-control" type="text" name="do_time" value="">
-                            </div>
+                    </div>
+                    <!--　送りに来た人 -->
+                    <div class="row align-items-center px-3 mb-3 do_guardian">
+                        <label for="do_guardian_id" class="form-label col-md-4 p-0 m-0">お送りに来た人：</label>
+                        <select class="form-select-lg p-2 col-md-8" name="do_guardian_id">
+                        </select>
+                    </div>
+                    <!-- 送りに来た時間 -->
+                    <div class="row px-3 align-items-center mb-3 do_time">
+                        <label for="do_time" class="col-form-label col-md-4 p-0">お送りに来た時間：</label>
+                        <div class="col-md-8 px-0">
+                            <input class="form-control p-2" type="text" name="do_time" value="">
                         </div>
-                        <!-- 迎えに来た人と時間 -->
-                        <div class="form-row">
-                            <!-- 迎えに来た人 -->
-                            <div class="form-group align-items-center col-md-6 pu_guardian">
-                                <label for="pu_guardian_id" class="col-form-label text-md-left">お迎えに来た人</label>
-                                <select class="form-control-md p-2" aria-label="Default select" name="pu_guardian_id">
-                                </select>
-                            </div>
-                            <!-- 迎えに来た時間 -->
-                            <div class="form-group align-items-center col-md-6 pu_time">
-                                <label for="pu_time" class="col-form-label text-md-left">お迎えに来た時間</label>
-                                <input class="form-control" type="text" name="pu_time" value="">
-                            </div>
+                    </div>
+
+
+                    <!-- 迎えに来た人 -->
+                    <div class="row align-items-center px-3 mb-3 pu_guardian">
+                        <label for="pu_guardian_id" class="form-label col-md-4 p-0 m-0">お送りに来た人：</label>
+                        <select class="form-select-lg p-2 col-md-8" name="pu_guardian_id">
+                        </select>
+                    </div>
+                    <!-- 迎えに来た時間 -->
+                    <div class="row px-3 align-items-center mb-3 pu_time">
+                        <label for="pu_time" class="col-form-label col-md-4 p-0">お送りに来た時間：</label>
+                        <div class="col-md-8 px-0">
+                            <input class="form-control p-2" type="text" name="pu_time" value="">
                         </div>
+                    </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary btn-close" data-bs-dismiss="modal">キャンセル</button>
-                    <button class="btn btn-primary btn-modal-submit" id="record-update">保存</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">キャンセル</button>
+                    <button class="btn btn-primary btn-modal-submit" id="record-update" value="">保存</button>
                 </div>
-                </form>
             </div>
         </div>
     </div>
@@ -130,51 +124,48 @@
         aria-labelledby="ModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <form class="mb-0">
-                    <div class="modal-body" id="modal-body">
-
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label for="date" class="record-date"></label>
-                            </div>
+                <div class="modal-body" id="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="date" class="record-date"></label>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6 kid_name">
-                                <label for="kid_name" class="text-md-left">クラス：</label>
-                                <p class="text-secondary record-grade"></p>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="kid_name" class="text-md-left">園児名：</label>
-                                <p class="text-secondary record-kid"></p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 do_guardian">
-                                <label for="do_guardian_name" class="text-md-left">お送りに来た人：</label>
-                                <p class="text-secondary" data-value="do_guardian"></p>
-                            </div>
-                            <div class="col-md-6 do_time">
-                                <label for="do_time" class="text-md-left">お送りに来た時間：</label>
-                                <p class="text-secondary"></p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 pu_guardian">
-                                <label for="pu_guardian_name" class="text-md-left">お迎えに来た人：</label>
-                                <p class="text-secondary" data-value="pu_guardian"></p>
-                            </div>
-                            <div class="col-md-6 pu_time">
-                                <label for="pu_time" class="text-md-left">お迎えに来た時間：</label>
-                                <p class="text-secondary"></p>
-                            </div>
-                        </div>
-                        <h5 class="modal-title" id="modalBasicLabel">レコードを削除しますか？</h5>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary btn-close" data-dismiss="modal">キャンセル</button>
-                        <button type="button" class="btn btn-primary btn-modal-submit" id="record-destroy">削除</button>
+                    <div class="row">
+                        <div class="col-md-6 kid_name">
+                            <label for="kid_name" class="text-md-left">クラス：</label>
+                            <p class="text-secondary record-grade"></p>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="kid_name" class="text-md-left">園児名：</label>
+                            <p class="text-secondary record-kid"></p>
+                        </div>
                     </div>
-                </form>
+                    <div class="row">
+                        <div class="col-md-6 do_guardian">
+                            <label for="do_guardian_name" class="text-md-left">お送りに来た人：</label>
+                            <p class="text-secondary" data-value="do_guardian"></p>
+                        </div>
+                        <div class="col-md-6 do_time">
+                            <label for="do_time" class="text-md-left">お送りに来た時間：</label>
+                            <p class="text-secondary"></p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 pu_guardian">
+                            <label for="pu_guardian_name" class="text-md-left">お迎えに来た人：</label>
+                            <p class="text-secondary" data-value="pu_guardian"></p>
+                        </div>
+                        <div class="col-md-6 pu_time">
+                            <label for="pu_time" class="text-md-left">お迎えに来た時間：</label>
+                            <p class="text-secondary"></p>
+                        </div>
+                    </div>
+                    <h5 class="modal-title" id="modalBasicLabel">レコードを削除しますか？</h5>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">キャンセル</button>
+                    <button type="button" class="btn btn-primary btn-modal-submit" id="record-destroy" value="">削除</button>
+                </div>
             </div>
         </div>
     </div>
